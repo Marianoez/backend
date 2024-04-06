7de10cd8129962205317177f20d2e42b72fa535c
Marianoez <marianoezequielvara@gmail.com>
2024-04-06 14:52:35 -0300

    Save me Samir

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const app = express();
 
 const ProductManager = new PManager();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Working perfectly");
 });
@@ -23,12 +26,33 @@ app.get("/productos", async (req, res) => {
 app.get("/productos/:id", async (req, res) => {
   await ProductManager.recovery();
   let id = req.params.id;
-  let prod = await ProductManager.getNameById(id);
+  let prod = await ProductManager.getProductById(id);
   console.log(prod);
 
   res.json(prod);
 });
 
+/* app.post("/productos", async (req, res) => {
+  try {
+    let newProduct = await ProductManager.addProduct({
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    });
+
+    res.setHeader("Content-Type", "aplication/json");
+    return res.status(200).json(newProduct);
+  } catch (error) {
+    res.setHeader("Content-Type", "aplication/json");
+    return res.status(500).json({
+      error: "Error en el servidor",
+    });
+  }
+}); */
+
 app.listen(PORT, () => {
-  console.log(`Server runing on Port ${PORT} `);
+  console.log(`Server runing on Port ${PORT}`);
 });
